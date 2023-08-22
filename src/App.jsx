226cf4be540fdfbecbd4d806d56ae7f0e7562cbd
mahziar saadat <mahzyarsaadat764@gmail.com>
2023-08-23 00:03:06 +0430

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+// import LoginPage from './components/pages/login-page'
+
+
+
+
+function App() {
+
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
